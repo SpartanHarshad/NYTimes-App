670215d895333033af35dd4617574bda0fa08fb3
SpartanHarshad <harshadpawar8555@gmail.com>
2021-04-13 11:57:24 +0530

    commit

---
+++ springbackend/src/test/java/com/nytimes/nytimes/NytimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nytimes.nytimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NytimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
